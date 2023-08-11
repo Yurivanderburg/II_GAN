af56ae9be75a4f64d9a8fb84d7cc035235edb18b
Yuri van der Burg <yuri.vanderburg@gmail.com>
2023-08-11 15:06:29 +0200

    apodfetch loops now overy the whole dataset; Tests.ipynb is now more tidy and includes rescaling to 64x64

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # II_Project
+
+This are the codes used for my research internship, where I try to apply machine-learning techniques in Intensity Interferometry. In particular, the aim is to create a generative neural network, which creates the light-source (e.g. a star) from the power spectrum. 
--- apodfetch.py
+++ apodfetch.py
@@ -13,26 +13,35 @@ def main():
         url_today = 'https://api.nasa.gov/planetary/apod?api_key=DB9DfUXnW0fZN4knCcghKyZ3f1GeKqTXkeymAhIr'
 
         # For a timeframe
+
+        years = np.arange(2012, 2021, 1)
         #start_date = "1995-06-16"
-        start_date = "2023-03-23"
-        url = f"https://api.nasa.gov/planetary/apod?api_key=DB9DfUXnW0fZN4knCcghKyZ3f1GeKqTXkeymAhIr&start_date={start_date}&end_date={today}"
 
-        all_results = requests.get(url).json()
+        # Loop over the years, so we only have 365 entries in each interation
+        for year in years:
+            image_counter = 0
+            start_date = f"{year}-03-11"
+            end_date = f"{year}-12-31"
 
-        # Check whether the words "galaxy" and "spiral" are in the description
-        for result in all_results:
-            date = result['date']
-            if ("spiral" and "galaxy") in result['explanation']:
-                print(f"APOD Image of {date} contains a spiral galaxy!")
+            url = f"https://api.nasa.gov/planetary/apod?api_key=DB9DfUXnW0fZN4knCcghKyZ3f1GeKqTXkeymAhIr&start_date=" \
+                  f"{start_date}&end_date={end_date}"
 
-                # Download images
-                image = requests.get(result['url']) # Change to hdurl if you want a high-resolution picture ;)
-                with open(f"/home/yuri/Documents/Uni/Master/Semester3/Internship/Data/SpiralGalaxy/apod_{date}.jpg", "wb") as f:
-                    f.write(image.content)
+            all_results = requests.get(url).json()
 
+            # Check whether the words "galaxy" and "spiral" are in the description
+            for result in all_results:
+                date = result['date']
+                if ("spiral" and "galaxy") in result['explanation']:
+                    image_counter += 1
+
+                    # Download images
+                    image = requests.get(result['url'])  # Change to hdurl if you want a high-resolution picture ;)
+                    with open(f"/home/yuri/Documents/Uni/Master/Semester3/Internship/Data/SpiralGalaxy/apod_{date}.jpg", "wb") as f:
+                        f.write(image.content)
+            print(f"Downloaded {image_counter} images from APOD in {year}")
 
     except:
-        return "URL Error"  # Return reload icon
+        return "URL Error"
 
 
 if __name__ == "__main__":
