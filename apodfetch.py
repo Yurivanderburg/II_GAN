22bad514859028e414833ea25eeba03b57504965
Yuri van der Burg <yuri.vanderburg@gmail.com>
2023-08-12 16:39:24 +0200

    Cleaned up apodfetch, including adding a docstring.

--- apodfetch.py
+++ apodfetch.py
@@ -1,44 +1,49 @@
 import requests
 import json
 import datetime
-import numpy as np
 
+# Params
+start_date = "2023-08-01"
+end_date = datetime.date.today()
 
+# Edit this to search for different images
+required_term1 = "spiral"
+required_term2 = "galaxy"
+
+do_download = True
 
 def main():
+    """
+    Python script that grabs several "Astronomy Pictures of the Day" (APOD), using APIs.
+    Can specify keywords which must be contained in the description of the image
+    Note: If the timescale is large (> 2 years), it would be wise to split the url, otherwise it might break.
+    """
     try:
-        # Get data from apod-api
-        # Only today:
-        today = datetime.date.today()
-        url_today = 'https://api.nasa.gov/planetary/apod?api_key=DB9DfUXnW0fZN4knCcghKyZ3f1GeKqTXkeymAhIr'
+        #
+        url = f"https://api.nasa.gov/planetary/apod?api_key=DB9DfUXnW0fZN4knCcghKyZ3f1GeKqTXkeymAhIr&start_date=" \
+              f"{start_date}&end_date={end_date}"
 
-        # For a timeframe
+        all_results = requests.get(url).json()
+        image_counter = 0 # Counts the images
 
-        years = np.arange(2012, 2021, 1)
-        #start_date = "1995-06-16"
+        # Check whether the words are in the description
+        for result in all_results:
+            date = result['date']
 
-        # Loop over the years, so we only have 365 entries in each interation
-        for year in years:
-            image_counter = 0
-            start_date = f"{year}-03-11"
-            end_date = f"{year}-12-31"
+            if (required_term1 and required_term2) in result['explanation']:
+                image_counter += 1
 
-            url = f"https://api.nasa.gov/planetary/apod?api_key=DB9DfUXnW0fZN4knCcghKyZ3f1GeKqTXkeymAhIr&start_date=" \
-                  f"{start_date}&end_date={end_date}"
-
-            all_results = requests.get(url).json()
-
-            # Check whether the words "galaxy" and "spiral" are in the description
-            for result in all_results:
-                date = result['date']
-                if ("spiral" and "galaxy") in result['explanation']:
-                    image_counter += 1
-
-                    # Download images
+                # Download images
+                if do_download:
                     image = requests.get(result['url'])  # Change to hdurl if you want a high-resolution picture ;)
-                    with open(f"/home/yuri/Documents/Uni/Master/Semester3/Internship/Data/SpiralGalaxy/apod_{date}.jpg", "wb") as f:
+                    with open(f"Data/SpiralGalaxy/apod_{date}.jpg", "wb") as f:
                         f.write(image.content)
-            print(f"Downloaded {image_counter} images from APOD in {year}")
+
+        if do_download:
+            print(f"Downloaded {image_counter} images between {start_date} and {end_date} matching the description.")
+        else:
+            print(f"Found {image_counter} images between {start_date} and {end_date} matching the description. "
+                  f"They were not downloaded.")
 
     except:
         return "URL Error"
