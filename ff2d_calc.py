b057d71baf90f3fefe0d71fe030483d62f55a95f
Yuri van der Burg <yuri.vanderburg@gmail.com>
2023-08-17 15:39:31 +0200

    Rewrote ff2d_calc and made a first runnable version of Spirals

--- ff2d_calc.py
+++ ff2d_calc.py
@@ -8,7 +8,7 @@ import os
 # Params
 image_size = 64 #px
 PATH = "Data/SpiralGalaxy/original"
-grayscale = False
+save_images = True
 
 
 def normalize(x):
@@ -20,6 +20,21 @@ def normalize(x):
     return np.array((x - np.min(x)) / (np.max(x) - np.min(x)))
 
 
+# Try to concat images
+def concat_images(img_a, img_b):
+    """
+    Combines two color image ndarrays side-by-side.
+    """
+    ha, wa = img_a.shape[:2]
+    hb, wb = img_b.shape[:2]
+    max_height = np.max([ha, hb])
+    total_width = wa+wb
+    new_img = np.zeros(shape=(max_height, total_width)) #, 1))
+    new_img[:ha, :wa] = img_a
+    new_img[:hb, wa:wa+wb] = img_b
+    return new_img
+
+
 def main():
     """
     Python script that calculates the 2D fourier transformation of all the spiral galaxy images, because these will be
@@ -28,54 +43,64 @@ def main():
     Bug 1: Grayscale does not reduce to 1 channel, but to 4.
     Bug 2: Non-grayscale image cannot be saved, because imsave requires normalized arrays. -> image distorted
     """
-
     counter = 0
-    # Loop through all images in the desired folder:
+
     for filename in os.listdir(PATH):
 
         file = os.path.join(PATH, filename)
         image_name = filename[:-4]
 
-        image_org = mpimg.imread(file)
+        # Convert to 1-channel grayscale and resize to 64px
+        img_org = mpimg.imread(file)
 
-        # Convert RGB to 1-channel grayscale and rescale
-        # try is necessary because some images might already be grayscale
-        if grayscale:
-            try:
-                img_gray = cv2.cvtColor(image_org, cv2.COLOR_BGR2GRAY)
-                img_processed = cv2.resize(img_gray, dsize=(image_size, image_size), interpolation=cv2.INTER_AREA)
-            except:
-                print(f"Image {filename} not RGB, so conversion failed.")
+        # Skip images that are already greyscale
+        try:
+            img_org = cv2.cvtColor(img_org, cv2.COLOR_BGR2GRAY)
+
+        except:
+            print(f"Image {image_name} not converted to grayscale.")
+
+        img_org_resized = cv2.resize(img_org, dsize=(64,64), interpolation=cv2.INTER_AREA)
+
+        # Calculate FF2D incl. Shift
+        img_fft = fft.fftshift(fft.fft2(fft.fftshift(img_org_resized)))
+        fft_argument = np.log(np.abs(img_fft))
+        img_fft_norm = fft_argument/np.max(fft_argument)
+
+        # Seems to be required, otherwise images are saved as black -> Unclear why ?
+        # img_org_resized = cv2.convertScaleAbs(img_org_resized, alpha=(255.0))
+        img_fft_norm = cv2.convertScaleAbs(img_fft_norm, alpha=(255.0))
+
+        # Need to combine image to deal with tensorflow
+        combined_image = concat_images(img_org_resized, img_fft_norm)
+
+        # Save or display images > use cv2 instead of matplotlib, as this always saves as (64,64,4)
+        # Already save them in test & train & validation datasets; seems random TODO: Improve/Cross-Check
+        if save_images:
+            if counter <= 150: # Save to val
+                cv2.imwrite(f"Data/SpiralGalaxy/val/{image_name}.jpg", combined_image)
+            elif (counter > 150) and (counter <= 550):
+                cv2.imwrite(f"Data/SpiralGalaxy/test/{image_name}.jpg", combined_image)
+            else:
+                cv2.imwrite(f"Data/SpiralGalaxy/train/{image_name}.jpg", combined_image)
 
         else:
-            img_processed = cv2.resize(image_org, dsize=(image_size, image_size), interpolation=cv2.INTER_AREA)
+            plt.imshow(img_org_resized)
+            print(img_org_resized.shape)
+            plt.show()
+            plt.imshow(img_fft_norm)
+            print(img_fft_norm.shape)
+            plt.show()
 
-        # Calculate 2D fourier transform (including fft-shifts)
-        img_fft = fft.fftshift(fft.fft2(fft.fftshift(img_processed)))
-
-        argument = np.log(np.abs(img_fft))
-        #argument[np.isneginf(argument)] = 0
-        #argument[np.isposinf(argument)] = 0
-        image_fft_norm = normalize(argument/np.max(argument))
-
-
-
-        # Save both images in different folders
-        if grayscale:
-            mpimg.imsave(f"Data/SpiralGalaxy/gray_rescaled/{image_name}_gray_resized.png", img_processed)
-            mpimg.imsave(f"Data/SpiralGalaxy/gray_ff2d/{image_name}_gray_fft_norm.png", image_fft_norm)
-                      #   cmap=plt.get_cmap('gray'))
-
-        else:
-            mpimg.imsave(f"Data/SpiralGalaxy/rescaled/{image_name}_resized.png", img_processed)
-            mpimg.imsave(f"Data/SpiralGalaxy/ff2d/{image_name}_fft_norm.png", image_fft_norm)
-
+            if counter == 10:
+                break
 
         counter += 1
-        if counter == 10:
-            break
 
-    print(f"{counter} images successfully converted.")
+    if save_images:
+        print(f"{counter} images successfully converted.")
+    else:
+        print(f"{counter} images not saved")
 
 
 if __name__ == "__main__":
