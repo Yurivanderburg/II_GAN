0680572e9a01da2fbc48781a8ace73f02103f6e3
Yuri van der Burg <yuri.vanderburg@gmail.com>
2023-08-18 15:14:34 +0200

    Created Spirals_v0.2; improved ff2d_calc; started tracking log files

--- ff2d_calc.py
+++ ff2d_calc.py
@@ -7,17 +7,8 @@ import os
 
 # Params
 image_size = 64 #px
-PATH = "Data/SpiralGalaxy/original"
+PATH = "Data/SpiralGalaxy/original_cleaned"
 save_images = True
-
-
-def normalize(x):
-    """
-    Normalize a list of sample image data in the range of 0 to 1
-    : x: List of image data.  The image shape is (32, 32, 3)
-    : return: Numpy array of normalized data
-    """
-    return np.array((x - np.min(x)) / (np.max(x) - np.min(x)))
 
 
 # Try to concat images
@@ -45,54 +36,67 @@ def main():
     """
     counter = 0
 
+    # Create folders if necessary
+    if save_images:
+        folders = ["val", "test", "train"]
+        for folder in folders:
+            if not os.path.exists(f"Data/SpiralGalaxy/{folder}"):
+                os.makedirs(f"Data/SpiralGalaxy/{folder}")
+                print(f"Directory Data/SpiralGalaxy/{folder} created.")
+
     for filename in os.listdir(PATH):
 
         file = os.path.join(PATH, filename)
         image_name = filename[:-4]
-
-        # Convert to 1-channel grayscale and resize to 64px
         img_org = mpimg.imread(file)
 
-        # Skip images that are already greyscale
+        ## Manipulate original images:
+        # Convert to 1-channel grayscale
+        # Resize images to image_size (skip images that are already grayscale)
+        # subtract mean > better contrast
+
         try:
             img_org = cv2.cvtColor(img_org, cv2.COLOR_BGR2GRAY)
-
         except:
             print(f"Image {image_name} not converted to grayscale.")
 
-        img_org_resized = cv2.resize(img_org, dsize=(64,64), interpolation=cv2.INTER_AREA)
+        img_org_ed = cv2.resize(img_org, dsize=(image_size, image_size), interpolation=cv2.INTER_AREA)
+        img_org_ed = img_org_ed - np.mean(img_org_ed)
 
-        # Calculate FF2D incl. Shift
-        img_fft = fft.fftshift(fft.fft2(fft.fftshift(img_org_resized)))
-        fft_argument = np.log(np.abs(img_fft))
+        ## Generate power spectrum from images
+        # Calculate 2D fourier transform (including shift)
+        # Take absolute value and normalize
+        img_fft = fft.fftshift(fft.fft2(fft.fftshift(img_org_ed)))
+        fft_argument = np.abs(img_fft)
         img_fft_norm = fft_argument/np.max(fft_argument)
 
         # Seems to be required, otherwise images are saved as black -> Unclear why ?
-        # img_org_resized = cv2.convertScaleAbs(img_org_resized, alpha=(255.0))
+        #img_org_ed = cv2.convertScaleAbs(img_org_ed, alpha=(255.0))
         img_fft_norm = cv2.convertScaleAbs(img_fft_norm, alpha=(255.0))
 
         # Need to combine image to deal with tensorflow
-        combined_image = concat_images(img_org_resized, img_fft_norm)
+        combined_image = concat_images(img_org_ed, img_fft_norm)
 
+        ## Create directories and save images:
         # Save or display images > use cv2 instead of matplotlib, as this always saves as (64,64,4)
         # Already save them in test & train & validation datasets; seems random TODO: Improve/Cross-Check
         if save_images:
-            if counter <= 150: # Save to val
+            if counter < 100: # Save to val
                 cv2.imwrite(f"Data/SpiralGalaxy/val/{image_name}.jpg", combined_image)
-            elif (counter > 150) and (counter <= 550):
+            elif (counter >= 100) and (counter < 200):
                 cv2.imwrite(f"Data/SpiralGalaxy/test/{image_name}.jpg", combined_image)
             else:
                 cv2.imwrite(f"Data/SpiralGalaxy/train/{image_name}.jpg", combined_image)
 
         else:
-            plt.imshow(img_org_resized)
-            print(img_org_resized.shape)
-            plt.show()
-            plt.imshow(img_fft_norm)
-            print(img_fft_norm.shape)
-            plt.show()
+            #plt.imshow(img_org_ed)
+            print("Original image: ", np.mean(img_org_ed))
+            #plt.show()
+            #plt.imshow(img_fft_norm)
+            print("FF2D image: ", np.mean(img_fft_norm))
+            #plt.show()
 
-            if counter == 10:
+            if counter == 2:
                 break
 
         counter += 1
