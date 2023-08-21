44037166c5a7d1e97d32769e296b589cb1743805
Yuri van der Burg <yuri.vanderburg@gmail.com>
2023-08-21 11:30:05 +0200

    Created generate_elipses.py

---
+++ generate_elipses.py
@@ -0,0 +1,64 @@
+import numpy as np
+import cv2
+from PIL import Image
+
+
+def main():
+    """
+    Function that generates ellipses of different size, angle and shape, and saves the images as .jpg images.
+    """
+
+    # Fixed parameters
+    image_size = 512
+    startAngle = 0
+    endAngle = 360
+    color = (255, 0, 0)
+    thickness = -1
+
+    # Variable parameters:
+    center_x = np.arange(100, 400, 30)  # 10
+    center_y = np.arange(100, 400, 30)  # 10
+    axesLength1 = np.arange(10, 220, 25)  # 9
+    axesLength2 = np.arange(10, 220, 25)  # 9
+    angles = np.arange(0, 360, 20)  # 15
+
+    iterations = (len(center_x) * len(center_y) * len(axesLength1) * len(axesLength2) * len(angles))
+    print(f"Generate almost {iterations} images...")
+
+    counter = 0
+
+    # Loop over variable parameters (center, axesLength, angle)
+    for x_pos in center_x:
+        for y_pos in center_y:
+            center = (x_pos, y_pos)
+            for axis1 in axesLength1:
+                for axis2 in axesLength2:
+                    axesLength = (axis1, axis2)
+                    for angle in angles:
+
+                        # Further conditions:
+                        # 1) Only rotate non-spherical shapes
+                        # 2) Not too eccentric
+
+                        if axis1 != axis2:
+                            if abs(axis1 - axis2) <= 150:
+
+                                new_image = np.zeros(shape=(image_size, image_size))
+                                image_ellipse = cv2.ellipse(new_image, center, axesLength, angle, startAngle, endAngle,
+                                                            color, thickness)
+                                # Save image
+                                image = Image.fromarray(image_ellipse.astype(np.uint8))
+                                image.save(f"Data/Shapes/tests/test_{counter}.jpg")
+
+                                # Print progress every 10'000 steps
+                                if (counter % 25000) == 0:
+                                    print(f"Progress: Step {counter}...")
+                                counter += 1
+
+    print(f"{counter} images were created")
+
+    return None
+
+
+if __name__ == "__main__":
+    main()
