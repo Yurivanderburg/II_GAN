9b02e97b60abbd2045255894855b922cbb3e3885
Yuri van der Burg <yuri.vanderburg@gmail.com>
2023-08-25 16:39:43 +0200

    Tested new ellipses with color gradients

--- generate_elipses.py
+++ generate_elipses.py
@@ -2,21 +2,25 @@ import numpy as np
 import cv2
 from PIL import Image
 import os
+import matplotlib.pyplot as plt
+from scipy.ndimage import rotate
 
 
 PATH = "Data/Ellipses/Tests"
+color_gradient = True
 
 
 def main():
     """
     Function that generates ellipses of different size, angle and shape, and saves the images as .jpg images.
+    The ellipses
     """
 
     # Fixed parameters
     image_size = 64
     startAngle = 0
     endAngle = 360
-    center = (int(image_size*0.5), int(image_size*0.5))
+    center = (int(image_size / 2), int(image_size / 2))
     color = (255, 0, 0)
     thickness = -1
 
@@ -24,42 +28,83 @@ def main():
     axesLength1 = np.arange(2, 32, 2)  # 15
     axesLength2 = np.arange(2, 32, 2)  # 15
     angles = np.arange(0, 360, 20)  # 15
-    
 
+    for axis in [axesLength1, axesLength2]:
+        if np.max(axis) > (image_size / 2):
+            raise ValueError("Ellipse should not be larger than the image")
+
+    # Create new directory (if it not already exists)
     if not os.path.exists(PATH):
         os.makedirs(PATH)
         print(f"Directory {PATH} created.")
 
+    # Estimate number of iterations
     iterations = (len(axesLength1) * len(axesLength2) * len(angles))
     print(f"Generate almost {iterations} images...")
 
-    counter = 0
-
     # Loop over variable parameters (center, axesLength, angle)
-
+    counter = 0
     for axis1 in axesLength1:
         for axis2 in axesLength2:
             axesLength = (axis1, axis2)
             for angle in angles:
 
-                # Further conditions:
-                # 1) Only rotate non-spherical shapes
-                # 2) Not too eccentric
+                # Condition: ellipse not too eccentric
+                if abs(axis1 - axis2) <= 20:
 
-                if axis1 != axis2:
-                    if abs(axis1 - axis2) <= 20:
-                        new_image = np.zeros(shape=(image_size, image_size))
+                    # Create ellipse
+                    new_image = np.zeros(shape=(image_size, image_size))
 
-                        image_ellipse = cv2.ellipse(new_image, center, axesLength, angle, startAngle, endAngle,
-                                                    color, thickness)
+                    image_ellipse = cv2.ellipse(new_image, center, axesLength, angle, startAngle, endAngle,
+                                                color, thickness)
+
+                    if color_gradient:
+                        # Create mask for color gradient
+                        upper_side = center[1] - axesLength[1]
+                        middle = center[1]
+                        lower_side = center[1] + axesLength[1]
+                        difference = (axesLength[1] + 1)
+                        col_grad = np.linspace(1, 1.5, difference)
+                        mask = np.ones((image_size, image_size))
+
+                        # Rotation
+                        if angle != 0:
+                            length = int(np.sqrt((upper_side / 2) ** 2 + (lower_side / 2) ** 2))
+                            col_grad_rot = np.linspace(1, 1.5, int(length))
+
+                            for i in range(image_size):
+                                mask[middle:middle + length, i] = col_grad_rot
+                                mask[middle - length:middle, i] = np.flip(col_grad_rot)
+
+                            # Rotate the mask and merge with image
+                            mask_rot = rotate(mask, angle=angle, reshape=False)
+                            image_ell_mask = np.multiply(mask_rot, image_ellipse)
+
+                        # No rotation
+                        else:
+                            for i in range(image_size):
+                                mask[middle:(lower_side + 1), i] = col_grad
+                                mask[upper_side:(middle + 1), i] = np.flip(col_grad)
+
+                            # Merge with the mask
+                            image_ell_mask = np.multiply(mask, image_ellipse)
+
                         # Save image
-                        image = Image.fromarray(image_ellipse.astype(np.uint8))
-                        image.save(f"{PATH}/ellipse_{counter}.jpg")
+                        image_ell_mask = image_ell_mask * (255 / np.max(image_ell_mask))
+                        image = Image.fromarray(image_ell_mask.astype(np.uint8))
 
-                        # Print progress every 1'000 steps
-                        if (counter % 1000) == 0:
-                            print(f"Progress: Step {counter}...")
-                        counter += 1
+                        # print("Image stat:", np.max(image), np.min(image))
+                        plt.imshow(image)
+
+                    # If not color_gradient:
+                    else:
+                        # image = Image.fromarray(image_ellipse.astype(np.uint8))
+                        plt.imshow(image_ellipse)
+
+                    # Print progress every 1'000 steps
+                    if (counter % 1000) == 0:
+                        print(f"Progress: Step {counter}...")
+                    counter += 1
 
     print(f"{counter} images were created")
 
