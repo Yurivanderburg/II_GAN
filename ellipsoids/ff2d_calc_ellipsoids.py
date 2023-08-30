30fc1db7da2a6e5179d696286c53f2a1267a7d2b
Yuri van der Burg <yuri.vanderburg@gmail.com>
2023-08-30 16:26:59 +0200

    Prepared Ellipsoids model for run

--- ellipsoids/ff2d_calc_ellipsoids.py
+++ ellipsoids/ff2d_calc_ellipsoids.py
@@ -7,9 +7,11 @@ import os
 
 # Params
 image_size = 64 #px
-PATH = "Data/Ellipsoids/original"
+PATH = "Data/original"
 save_images = True
 grayscale = True
+SAP_noise = True
+alpha = 0.05 # Salt and Pepper Noise probability
 
 
 # Try to concat images
@@ -27,13 +29,34 @@ def concat_images(img_a, img_b):
     return new_img
 
 
+def saltandpepper(image, prob):
+    """
+    Add salt and pepper noise to image
+    prob: Probability of the noise
+    """
+    output = image.copy()
+    if len(image.shape) == 2:
+        black = 0
+        white = 255
+    else:
+        colorspace = image.shape[2]
+        if colorspace == 3:  # RGB
+            black = np.array([0, 0, 0], dtype='uint8')
+            white = np.array([255, 255, 255], dtype='uint8')
+        else:  # RGBA
+            black = np.array([0, 0, 0, 255], dtype='uint8')
+            white = np.array([255, 255, 255, 255], dtype='uint8')
+    probs = np.random.random(output.shape[:2])
+    output[probs < (prob / 2)] = black
+    output[probs > 1 - (prob / 2)] = white
+
+    return output
+
+
 def main():
     """
-    Python script that calculates the 2D fourier transformation of all the spiral galaxy images, because these will be
+    Python script that calculates the 2D fourier transformation of all the ellipsoids, because these will be
     the input images for the NN.
-
-    Bug 1: Grayscale does not reduce to 1 channel, but to 4.
-    Bug 2: Non-grayscale image cannot be saved, because imsave requires normalized arrays. -> image distorted
     """
     counter = 0
 
@@ -41,13 +64,19 @@ def main():
     if save_images:
         folders = ["val", "test", "train"]
         for folder in folders:
-            if not os.path.exists(f"Data/Ellipses/{folder}"):
-                os.makedirs(f"Data/Ellipses/{folder}")
-                print(f"Directory Data/Ellipses/{folder} created.")
+            if not os.path.exists(f"Data/Ellipsoids/SAP/{folder}"):
+                os.makedirs(f"Data/Ellipsoids/SAP/{folder}")
+                print(f"Directory Data/Ellipsoids/SAP/{folder} created.")
 
     for filename in os.listdir(PATH):
 
+        # Load image
         file = os.path.join(PATH, filename)
+
+        # Ignore .directory
+        if filename == ".directory":
+            continue
+
         image_name = filename[:-4]
         img_org = mpimg.imread(file)
 
@@ -58,35 +87,47 @@ def main():
             except:
                 print(f"Image {image_name} not converted to grayscale.")
 
+        # (Optional) Calculate Salt and Pepper noise
+        if SAP_noise:
+            noise = saltandpepper(img_org, alpha)
+            img_org = cv2.add(img_org, noise)
+            label = "_noise"
+        else:
+            label = ""
+
         # Resize to image_size and subtract the mean
         img_org_ed = cv2.resize(img_org, dsize=(image_size, image_size), interpolation=cv2.INTER_AREA)
         img_org_ed = img_org_ed - np.mean(img_org_ed)
 
-
-        ## Generate power spectrum from images
-        # Calculate 2D fourier transform (including shift)
-        # Take absolute value and normalize
+        # Calculate 2D FFT and normalize
         img_fft = fft.fftshift(fft.fft2(fft.fftshift(img_org_ed)))
         fft_argument = np.abs(img_fft)
         img_fft_norm = fft_argument/np.max(fft_argument)
 
         # Seems to be required, otherwise images are saved as black -> Unclear why ?
-        #img_org_ed = cv2.convertScaleAbs(img_org_ed, alpha=(255.0))
         img_fft_norm = cv2.convertScaleAbs(img_fft_norm, alpha=(255.0))
 
         # Need to combine image to deal with tensorflow
         combined_image = concat_images(img_org_ed, img_fft_norm)
 
+        # If noise is on, we need to combine it also with the original image (w/o noise):
+        if SAP_noise:
+            img_nonoise = mpimg.imread(file)
+            img_nonoise = cv2.cvtColor(img_nonoise, cv2.COLOR_BGR2GRAY)
+            img_nonoise = cv2.resize(img_nonoise, dsize=(image_size, image_size), interpolation=cv2.INTER_AREA)
+            img_nonoise = img_nonoise - np.mean(img_nonoise)
+            combined_image = concat_images(img_nonoise, combined_image)
+
         ## Create directories and save images:
         # Save or display images > use cv2 instead of matplotlib, as this always saves as (64,64,4)
         # Already save them in test & train & validation datasets; seems random TODO: Improve/Cross-Check
         if save_images:
-            if counter < 600: # Save to val
-                cv2.imwrite(f"Data/Ellipsoids/val/{image_name}.jpg", combined_image)
-            elif (counter >= 600) and (counter < 1200):
-                cv2.imwrite(f"Data/Ellipsoids/test/{image_name}.jpg", combined_image)
+            if counter < 300: # Save to val
+                cv2.imwrite(f"Data/Ellipsoids/SAP/val/{image_name}{label}.jpg", combined_image)
+            elif (counter >= 300) and (counter < 600):
+                cv2.imwrite(f"Data/Ellipsoids/SAP/test/{image_name}{label}.jpg", combined_image)
             else:
-                cv2.imwrite(f"Data/Ellipsoids/train/{image_name}.jpg", combined_image)
+                cv2.imwrite(f"Data/Ellipsoids/SAP/train/{image_name}{label}.jpg", combined_image)
 
         else:
             plt.imshow(img_org_ed)
--- ellipsoids/generate_elllipsoids.py
+++ ellipsoids/generate_elllipsoids.py
@@ -1,11 +1,11 @@
 import numpy as np
 import os
 import matplotlib.pyplot as pl
+import time
 from functions import grids, ellip
 
 
-
-PATH = "Data/Tests"
+PATH = "Data/Tests3"
 color_gradient = True
 
 
@@ -15,44 +15,63 @@ def main():
     """
 
     # Fixed parameters
-    image_size = 64
     sx, sy, x, y = grids(1e-10, 512, 1e-6)
+    epsilon = 0.07
 
 
     # Variable parameters:
+    ## What has been used priviously:
+    #     rad_ = np.arange(3e-9, 1.6e-8, 2e-9) # 7
+    #     inclination_ = np.arange(0,2*np.pi, np.pi/4)  # 8
+    #     pa_ = np.arange(0, 2*np.pi, np.pi/4)  # 8
+    #     sq_ = np.arange(0.6, 1.6, 0.1)  # 10
+
     # Ellipse inputs: (sx,sy,rad,inc,pa,sq)
     # Rad: between 3e-9 and 1.5e-8
     # Inclination: between 0 and 2pi? (artifacts for e.g. 1.57!!!)
     # pa: Rotation around (x/y axis (not z axis!!); between 0 and 2pi
     # sq: "thickness" in z-direction of the ellipsoid (pole-pole distance). Best between 0.5 and 1.2
+
     rad_ = np.arange(3e-9, 1.6e-8, 2e-9) # 7
-    inclination_ = np.arange(0, 2*np.pi, np.pi/4)  # 8
+    #inclination_ = np.arange(0, 2*np.pi, np.pi/4)  # 4
+    inclination_ = [(1/2)*np.pi, (3/2)*np.pi]
     pa_ = np.arange(0, 2*np.pi, np.pi/4)  # 8
-    sq_ = np.arange(0.5, 1.2, 0.1)  # 7
+    sq_ = np.arange(0.6, 1.6, 0.1)  # 10
+
+
 
     # Create new directory (if it not already exists)
     if not os.path.exists(PATH):
         os.makedirs(PATH)
         print(f"Directory {PATH} created.")
 
+    # Estimate number of images:
+    print(f"Generating {len(rad_)*len(inclination_)*len(pa_)*len(sq_)} images...")
+
+    start = time.time()
+
     # Loop over variable parameters (center, axesLength, angle)
     counter = 0
     for rad in rad_:
         for inclination in inclination_:
+
+            # Add a small deviation, to dodge the singularities
+            if (inclination == (1 / 2) * np.pi) or (inclination == (3 / 2) * np.pi):
+                inclination += epsilon
+
             for pa in pa_:
                 for sq in sq_:
 
                     # Draw ellipse
-
                     ellipse = ellip(sx,sy,rad,inclination,pa,sq)
-                    #image = Image.fromarray(ellipse.astype(np.uint8))
                     pl.imsave(f"{PATH}/ellipsoid_{counter}.jpg", ellipse)
 
-                    # Print progress every 1'000 steps
-                    if (counter % 100) == 0:
-                        print(f"Progress: Step {counter} ___________________________")
+                    # Print progress every 50 steps
+                    if (counter % 50) == 0:
+                        if counter != 0:
+                            print(f"Progress: Step {counter}. Last 50 steps took {time.time()-start:.2f} seconds.")
+                            start = time.time()
                     counter += 1
-
 
     print(f"{counter} images were created")
 
