56aa58d802404b2e673c73ecb2c4e27307ad9af7
KNRai <niturai20129617@iisertvm.ac.in>
2025-06-05 11:02:54 +0530

    baseline

--- Phase_II/code/generate_baseline.py
+++ Phase_II/code/generate_baseline.py
@@ -10,21 +10,23 @@ obs = observe()
 vb = vary_base()
 
 # observation time for Intensity Interferometry
-start = [2460311.20167, 2460312.20167, 2460313.20167]   # (Jan 1-3, 2024, 7pm)
-end = [2460311.70833, 2460312.70833, 2460313.70833]     # (Jan 2-4, 2024, 5am)
+start = [2460311.20167]#, 2460312.20167, 2460313.20167, 2460314.20167, 2460315.20167]   # (Jan 1-5, 2024, 7pm)
+end = [2460311.70833]#, 2460312.70833, 2460313.70833, 2460314.70833, 2460315.70833]     # (Jan 2-6, 2024, 5am)
 step = obs.obslen(start, end, 0.0104167)
 jd = obs.julday(start, end, step)
 
 # telescope position, name, baseline name and baseline length
-# position of telescope on earth, Hanle Ladhakh
-la = vb.raddeg(32, 46, 48) 
-lo = vb.raddeg(78, 58, 35)
+# the position of Veritas Observatory
+la = vb.raddeg(31, 40, 30) 
+lo = vb.raddeg(110, 57, 7)
 
 # coordinates for telescope
 x = np.linspace(-60, 150, 500)
 y = np.linspace(-60, 80, 500)
 Tname = ['T1', 'T2', 'T3', 'T4'] 
 Tpos = [135 - 15j, 40 - 50j, 30 + 60j, -40 + 10j]
+ang = np.exp(0.1j)
+Tpos = [ang*x for x in Tpos]
 fname = 'telescope.png'
 ap.telescope(Tpos, Tname, fname, x, y, 6)
 
@@ -34,9 +36,9 @@ x = np.real(base)
 y = np.imag(base)
 z = 1e-6
 
-# position of source Polaris (assumed) in sky coordinate
-r = vb.radhr(2, 31, 49.09)
-de = vb.raddeg(89, 15, 50.8)
+# a single star at the position of source Spica
+r = vb.radhr(13, 25, 11.579)
+de = vb.raddeg(-11, 9, 40.75)
 
 # variational baseline according to the earth rotation
 vb.ps_para(lat=la, lon=lo, rac=r, dec=de)
@@ -52,16 +54,21 @@ zt = distance[:,2]           # baseline in up
 
 # plot the covered (u,v) plane with baselines
 plt.close()
-plt.rcParams.update({'font.size': 14})
-plt.rcParams["figure.figsize"] = [14,12]
+plt.rcParams.update({'font.size': 15})
+plt.rcParams["figure.figsize"] = [12,8]
 plt.rcParams["font.weight"] = "bold"
 plt.rcParams["axes.labelweight"] = "bold"
 st = np.cumsum(step)
+color = ['cornflowerblue', 'orange', 'cyan', 'olive', 'red', 'purple']
 for k in range(0, len(xt), 1):
-    plt.plot(xt[k,:], yt[k,:], '.', color="black")
-    plt.axis('off')
-    plt.gca().set_aspect('equal')
-plt.savefig('base/baseline.png', bbox_inches='tight', pad_inches=0, dpi=500)
+    plt.plot(xt[k,:], yt[k,:], 'o', markeredgecolor='blue', markersize=6, markeredgewidth=1.5, color=color[k], )
+    
+#plt.axis('off')
+plt.gca().set_aspect('equal')
+plt.xlabel('Along East in meter')
+plt.ylabel('Along North in meter')
+plt.title('The covered observational (u, v) plane', fontweight='bold')
+plt.savefig('base/baseline.png')#, bbox_inches='tight', pad_inches=0, dpi=500)
 
 
 # Convert the observational plane to grayscale and re-arrange pixels and save the baseline in .npy format
@@ -72,4 +79,5 @@ mask = np.where(mask < 1, 1, 0).astype(np.float32)                              
 mask = cv2.resize(mask, dsize=(image_size, image_size), interpolation=cv2.INTER_AREA) # gives the best results
 mask = np.where(mask > 0, 1, 0)
 np.save("base_npy/base", mask)                                                        # Save mask as numpy array
+
 
